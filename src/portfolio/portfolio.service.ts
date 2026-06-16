b2cb45191f5bb478a101b1902db923f5c248cd08
maron09 <chimarokeonyebi@gmail.com>
2026-06-16 21:42:16 +0100

    fix: yahoo-finance2 V3 instantiation

--- src/portfolio/portfolio.service.ts
+++ src/portfolio/portfolio.service.ts
@@ -1,8 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common'
 import { PrismaService } from 'prisma/prisma.service'
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const yahooFinance = require('yahoo-finance2').default
-
+import YahooFinance from 'yahoo-finance2'
+const yahooFinance = new YahooFinance()
 @Injectable()
 export class PortfolioService {
   constructor(private prisma: PrismaService) {}
